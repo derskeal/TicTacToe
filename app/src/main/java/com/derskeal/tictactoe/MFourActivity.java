4ff1b3250e91c099c86937ad9d87361f48be34b1
derskeal <27731088+derskeal@users.noreply.github.com>
2018-04-03 18:50:01 +0100

    Added activity for 4x4 box

--- app/src/main/java/com/derskeal/tictactoe/MFourActivity.java
+++ app/src/main/java/com/derskeal/tictactoe/MFourActivity.java
@@ -28,7 +28,7 @@ public class MFourActivity extends AppCompatActivity {
             public void onClick(View view) {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
-                //reset_game();
+                reset_game();
             }
         });
     }
