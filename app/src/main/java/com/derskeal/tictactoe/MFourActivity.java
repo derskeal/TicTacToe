17e9fa34d391a52c28b23cccb8fb50be2fcded5c
derskeal <27731088+derskeal@users.noreply.github.com>
2018-04-03 17:42:26 +0100

    Added activity for 4x4 box

---
+++ app/src/main/java/com/derskeal/tictactoe/MFourActivity.java
@@ -0,0 +1,13 @@
+package com.derskeal.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mfour);
+    }
+}
