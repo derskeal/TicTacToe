55e8325ea54a6017a272d492e255c75756c47fcb
derskeal <27731088+derskeal@users.noreply.github.com>
2018-04-05 12:21:19 +0100

    Added activity for single player 3x3 box - Computer plays(but dumb)

---
+++ app/src/main/java/com/derskeal/tictactoe/ScoreBoardActivity.java
@@ -0,0 +1,15 @@
+package com.derskeal.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_board);
+
+
+    }
+}
